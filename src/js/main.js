9c95b4146ae4a8ef97ffa893440154580ca6bb81
Webpolka <webpolka@gmail.com>
2025-09-18 14:42:02 +0300

    fixik-finish

--- src/js/main.js
+++ src/js/main.js
@@ -45,35 +45,136 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 /* -------------------------------------------------------------------------------------------------------------------------------------------------
-Video Modal
+MOdal listener
 -----------------------------------------------------------------------------------------------------------------------------------------------------*/
 document.addEventListener("DOMContentLoaded", () => {
-	const modal = document.getElementById("videoModal");
-	const allOpenBtns = document.querySelectorAll("[data-video]");
-	const closeBtn = document.getElementById("closeModal");
+	const header = document.querySelector(".header");
+	const allOpenVideoBtns = document.querySelectorAll("[data-video]");
+	const allCloseBtns = document.querySelectorAll("[data-modal='close']");
+	const openFormModalBtns = document.querySelectorAll("[data-modal='open']");
+	const videoModal = document.querySelector("#videoModal");
+	const formModal = document.querySelector("#formModal");
 
-	// Открытие модального окна
-	allOpenBtns.forEach((btn) => {
+	const thanksModal = document.querySelector("#thanksModal");
+	const openThanksModalBtn = document.querySelector("[data-modal='thanks']");
+
+	const allModals = [];
+	formModal && allModals.push(formModal);
+	videoModal && allModals.push(videoModal);
+	thanksModal && allModals.push(thanksModal);
+
+	if (openThanksModalBtn)
+		openThanksModalBtn.onclick = (e) => {
+			e.preventDefault();
+			console.log("Thanks");
+			closeAllModals();
+			setTimeout(() => {
+				openModal(thanksModal);
+			}, 300);
+		};
+
+	// Открытие модального VIDEO окна
+	allOpenVideoBtns.forEach((btn) => {
 		btn.onclick = (e) => {
 			e.preventDefault();
 			e.stopPropagation();
-			modal.classList.add('modal--show');
-      document.documentElement.classList.add('no-scroll');
+			const url = btn.dataset.video;
+			const previewUrl = btn.nextElementSibling ? btn.nextElementSibling : null;			
+			openModal(videoModal, url, previewUrl ? previewUrl.src : null);
 		};
 	});
 
-	// Закрытие модального окна
-	closeBtn.onclick = (e) => {
-		e.preventDefault();	
-		modal.classList.remove('modal--show');
-    document.documentElement.classList.remove('no-scroll');
-	};
+	// Открытие модального FORM окна
+	openFormModalBtns.forEach((btn) => {
+		btn.onclick = (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			openModal(formModal);
+		};
+	});
+
+	allCloseBtns.forEach((btn) => {
+		btn.onclick = (e) => {
+			e.stopPropagation();
+			e.preventDefault();
+			closeAllModals();
+		};
+	});
+
+	function closeAllModals() {
+		allModals.forEach((modal) => {
+			modal.classList.remove("modal--show");
+
+			const existingVideo = modal.querySelector("video");
+			if (existingVideo) {
+				existingVideo.pause();
+				existingVideo.remove();
+			}
+		});
+		removeNoScroll();
+	}
+
+	function addNoScroll() {
+		const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+		document.documentElement.style.paddingRight = scrollbarWidth + "px";
+		header.style.paddingRight = scrollbarWidth + "px";
+
+		header.classList.add("no-scroll");
+		document.documentElement.classList.add("no-scroll");
+	}
+	function removeNoScroll() {
+		header.classList.remove("no-scroll");
+		document.documentElement.classList.remove("no-scroll");
+
+		document.documentElement.style.removeProperty("padding-right");
+		header.style.removeProperty("padding-right");
+	}
+
+	function openModal(modal, url, previewUrl) {
+		// form modal open
+		if (formModal && modal === formModal) {
+			modal.classList.add("modal--show");
+			addNoScroll();
+			console.log("Form modal");
+		} else if (thanksModal && modal === thanksModal) {
+			modal.classList.add("modal--show");
+			addNoScroll();
+			console.log("Thanks modal");
+			// video modal open
+		} else if (videoModal && modal === videoModal) {
+			modal.classList.add("modal--show");
+			addNoScroll();
+
+			const container = modal.querySelector(".next-video-modal__forvideo");
+			// Создать элемент видео
+			const video = document.createElement("video");
+			video.src = url;
+			video.controls = true; // показать элементы управления
+			video.autoplay = true; // запускать автоматически
+			video.style.width = "100%"; // по желанию
+			previewUrl && video.setAttribute("poster", previewUrl);
+			// Обработчик события окончания видео
+			video.addEventListener("ended", () => {
+				// Запустить таймер на 23 секунды
+				setTimeout(() => {
+					// Удалить видео из контейнера
+					container.innerHTML = "";
+					closeAllModals();
+				}, 500);
+			});
+
+			// Вставить видео в контейнер
+			container.appendChild(video);
+			console.log("Video modal --", url);
+		}
+	}
 
 	// Закрытие при клике вне окна
 	window.onclick = function (event) {
-		if (event.target == modal) {
-			modal.classList.remove('modal--show');
-      document.documentElement.classList.remove('no-scroll');
-		}
+		allModals.forEach((modal) => {
+			if (event.target == modal && event.target != videoModal) {
+				closeAllModals();
+			}
+		});
 	};
 });
